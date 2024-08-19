984400a247d31756c3d3cec184db604050f8ae47
Clanka-Clanker <asaduqailipro321@gmail.com>
2024-08-19 18:33:09 -0400

    LAB4

--- Super Mario_Something/Sponge_booob/Assets/Enemies/Pickup.cs
+++ Super Mario_Something/Sponge_booob/Assets/Enemies/Pickup.cs
@@ -4,30 +4,47 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum PickupType
     {
-        
+        Life,
+        PowerupJump,
+        PowerupSpeed,
+        Score
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private PickupType type;
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        Destroy(gameObject);
+    //        // Perform actions when the player collects the object
+    //        // Add score, play a sound, or activate a power-up
+    //        //Collect();
+    //    }
+    //}
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        
-    }
-
-
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
+        if (collider.CompareTag("Player"))
         {
+            NewBehaviourScript pc = collider.GetComponent<NewBehaviourScript>();
+
+            switch (type)
+            {
+                case PickupType.Life:
+                    pc.lives++;
+                    break;
+                case PickupType.PowerupJump:
+                case PickupType.PowerupSpeed:
+                    pc.PowerupValueChange(type);
+                    break;
+                case PickupType.Score:
+                    break;
+
+            }
             Destroy(gameObject);
         }
-
     }
-
-
-
 
 }
--- Super Mario_Something/Sponge_booob/Assets/Script Kiddies/NewBehaviourScript.cs
+++ Super Mario_Something/Sponge_booob/Assets/Script Kiddies/NewBehaviourScript.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
@@ -11,6 +13,109 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField, Range(1, 20)] private float jumpForce = 10;
     [SerializeField, Range(0.01f, 1)] private float groundCheckRadius = 0.02f;
     [SerializeField] private LayerMask isGroundLayer;
+
+
+    //Player gameplay variables
+    private Coroutine jumpForceChange;
+    private Coroutine speedChange;
+
+    public void PowerupValueChange(Pickup.PickupType type)
+    {
+        if (type == Pickup.PickupType.PowerupSpeed)
+            StartPowerupCoroutine(ref speedChange, ref speed, type);
+
+        if (type == Pickup.PickupType.PowerupJump)
+            StartPowerupCoroutine(ref jumpForceChange, ref jumpForce, type);
+
+    }
+
+    public void StartPowerupCoroutine(ref Coroutine InCoroutine, ref float inVar, Pickup.PickupType type)
+    {
+        if (jumpForceChange != null)
+        {
+            StopCoroutine(InCoroutine);
+            InCoroutine = null;
+            inVar /= 2;
+
+        }
+
+        InCoroutine = StartCoroutine(PowerupChange(type));
+    }
+
+    IEnumerator PowerupChange(Pickup.PickupType type)
+    {
+        //this code runs before the wait
+        if (type == Pickup.PickupType.PowerupSpeed)
+            speed *= 2;
+
+        if (type == Pickup.PickupType.PowerupJump)
+            jumpForce *= 2;
+
+
+        Debug.Log($"Jump force value is {jumpForce}, Speed Value is {speed}");
+
+        yield return new WaitForSeconds(5.0f);
+
+        //this code runs after the wait
+        if (type == Pickup.PickupType.PowerupSpeed)
+        {
+            speed /= 2;
+            speedChange = null;
+        }
+        if (type == Pickup.PickupType.PowerupJump)
+        {
+            jumpForce /= 2;
+            jumpForceChange = null;
+        }
+
+
+        Debug.Log($"Jump force value is {jumpForce}");
+
+
+    }
+
+    //Private Lives Variable
+    private int _lives = 5;
+
+    //public variable for getting and setting lives
+
+    public int lives
+    {
+        get
+        {
+            return _lives;
+        }
+        set
+        {
+            //all lives lost (zero counts as a life due to the check)
+            if (value < 0)
+            {
+                //game over function called here
+                //return to prevent the rest of the function to be called
+                return;
+            }
+
+            //lost a life
+            if (value < _lives)
+            {
+                //Respawn function called he re
+
+            }
+
+            if (value > maxLives)
+            {
+                value = maxLives;
+            }
+
+            _lives = value;
+
+            Debug.Log($"Lives value on {gameObject.name} has changed to {lives}");
+        }
+    }
+
+    //max lives possible
+    [SerializeField] private int maxLives = 10;
+
 
     //GroundCheck Stuff
     private Transform groundCheck;
